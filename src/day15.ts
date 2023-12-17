031d717bee80969b75650f395b5ed3de52ac0a93
Ahmed Kamal <ahmed@moon-i.com.au>
2023-12-17 14:37:14 +1100

    add day 15

---
+++ src/day15.ts
@@ -0,0 +1,18 @@
+type BoxToys<T, L extends number, P extends T[] = []> = L extends P['length']
+  ? P
+  : BoxToys<T, L, [T, ...P]>;
+
+/************************** TESTS **************************/
+import { Expect, Equal } from 'type-testing';
+
+type test_doll_actual = BoxToys<'doll', 1>;
+//   ^?
+type test_doll_expected = ['doll'];
+type test_doll = Expect<Equal<test_doll_expected, test_doll_actual>>;
+
+type test_nutcracker_actual = BoxToys<'nutcracker', 3 | 4>;
+//   ^?
+type test_nutcracker_expected =
+  | ['nutcracker', 'nutcracker', 'nutcracker']
+  | ['nutcracker', 'nutcracker', 'nutcracker', 'nutcracker'];
+type test_nutcracker = Expect<Equal<test_nutcracker_expected, test_nutcracker_actual>>;
